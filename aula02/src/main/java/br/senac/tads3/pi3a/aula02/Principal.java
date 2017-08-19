f25e6b95201bbbe4eed1ad94a11133bd5f2301b6
danilo.rcoimbra <danilo.rcoimbra@CAS555491W0728.senacsp.edu.br>
2017-08-18 21:40:48 -0300

    Versão Inicial

---
+++ aula02/src/main/java/br/senac/tads3/pi3a/aula02/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads3.pi3a.aula02;
+
+/**
+ *
+ * @author danilo.rcoimbra
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Eai");
+    }
+    
+}
